121683fdcc4b04076fa33d6fc907cca70482a4e5
Florian Kimmel <flo.kimmel@gmail.com>
2026-01-01 12:17:39 +0100

    [chore] format

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { router } from './router.tsx'
-import { RouterProvider } from 'react-router/dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { router } from './router.tsx';
+import { RouterProvider } from 'react-router/dom';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
